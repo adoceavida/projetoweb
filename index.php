51991cd6d69b5d004195b3965d89c7589f622de3
adoceavida <natmuehringer@gmail.com>
2015-09-19 01:50:39 -0300

    Criação de Layout

--- index.php
+++ index.php
@@ -1,17 +1,76 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>:: Adoce a Vida - Bolos & Doces Caseiros ::</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Adoce a Vida</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#" data-toggle="tooltip" data-placement="bottom" title="Logo"><img alt="Logo" src="..."></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li ><a href="#" data-toggle="tooltip" data-placement="bottom" title="Home">Home</a></li>
+                    <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Sobre Nós">Sobre Nós</a></li>
+                    <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Bolos & Doces">Bolos & Doces</a></li>
+                    <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Localização">Localização</a></li>
+                    <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Contato">Contato</a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        
+        
+        ksjdlkasjd
+        sadasd
+        asd
+        sad
+        sad
+        sd
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        
+        <script src="js/jquery-1.11.3.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+        <script src="js/ie10-viewport-bug-workaround.js" type="text/javascript"></script>
+        <script>
+            $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+            </script>
     </body>
 </html>
