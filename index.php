2cff5a6942e48c459b330804480083a8a026121d
adoceavida <natmuehringer@gmail.com>
2015-09-29 00:16:54 -0300

    Ajuste do carrosel

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
         <![endif]-->
     </head>
     <body>
-        <div class="cabecalho">
         <nav class="navbar navbar-default navbar-fixed-top">
           <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
@@ -55,31 +54,47 @@
                 </div><!-- /.navbar-collapse -->
           </div><!-- /.container-fluid -->
         </nav>
-        </div>
-        <div><br><br><br><br><br><br><br>teste</div>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        <br><br><br><br><br><br>
-        
+
+        <div id="carouselHome" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselHome" data-slide-to="1"></li>
+              <li data-target="#carouselHome" data-slide-to="2"></li>
+            </ol>
+            
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                  <img src="http://creativefan.com/files/2010/10/Dexter_Apple_Wallpaper_by_WhoIsScott-500x500.jpg" alt="teste"  width="500px">
+                <div class="carousel-caption">
+                   teste
+                </div>
+              </div>
+              <div class="item">
+                  <img src="http://creativefan.com/files/2010/10/Dexter_Apple_Wallpaper_by_WhoIsScott-500x500.jpg" alt="..." width="500px">
+                <div class="carousel-caption">
+                    teste1
+                </div>
+              </div>
+              <div class="item">
+                  <img src="http://creativefan.com/files/2010/10/Dexter_Apple_Wallpaper_by_WhoIsScott-500x500.jpg" alt="..." width="500px">
+                <div class="carousel-caption">
+                    teste2
+                </div>
+              </div>
+            </div>
+            
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carouselHome" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carouselHome" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>     
         
         <script src="js/jquery-1.11.3.js" type="text/javascript"></script>
         <script src="js/bootstrap.js" type="text/javascript"></script>
